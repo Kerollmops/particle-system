ed18acacca0b80f9e1612ceb29c5ee6465a13b78
Clement RENAULT <crenault@e2r2p4.42.fr>
2016-02-12 12:04:08 +0100

    throw chnaged

--- src/Window.cpp
+++ src/Window.cpp
@@ -6,13 +6,14 @@
 /*   By: crenault <crenault@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/11 13:05:03 by crenault          #+#    #+#             */
-/*   Updated: 2016/02/11 13:30:39 by crenault         ###   ########.fr       */
+/*   Updated: 2016/02/11 15:57:53 by crenault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Window.hpp"
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <exception>
 
 void		glfw_error_callback(int error, const char *description)
@@ -20,28 +21,25 @@ void		glfw_error_callback(int error, const char *description)
 	dprintf(2, "GLFW Error: code %i msg: %s\n", error, description);
 }
 
-int			init_gl_context(void)
+void			init_gl_context(void)
 {
 	glfwSetErrorCallback(glfw_error_callback);
 	if (glfwInit() == 0)
 	{
 		dprintf(2, "ERROR: could not start GLFW3\n");
-		return (1);
+		throw std::exception();
 	}
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_SAMPLES, 4);
-	return (0);
 }
 
 Window::Window(const char *title, int width, int height, int resizable)
 	: _title(strdup(title)), _resizable(resizable), _width(width), _height(height) {
 
-	if (init_gl_context() != 0) {
-		throw std::exception();
-	}
+	init_gl_context();
 	glfwWindowHint(GLFW_RESIZABLE, resizable);
 	this->_window_ptr = glfwCreateWindow(this->_width, this->_height, this->_title, NULL, NULL);
 	glfwGetWindowSize(this->_window_ptr, &this->_width, &this->_height);
@@ -62,6 +60,7 @@ Window::Window(Window const &src) {
 
 Window::~Window(void) {
 
+	free(this->_title);
 	glfwMakeContextCurrent(NULL);
 	glfwDestroyWindow(this->_window_ptr);
 }
