3bd19023b4ea87b270c914c60a9aba5011b3db4b
Clement RENAULT <crenault@e2r8p11.42.fr>
2016-02-13 17:23:39 +0100

    need to create vao/vbos

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,33 @@
 /*   By: crenault <crenault@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/11 11:41:05 by crenault          #+#    #+#             */
-/*   Updated: 2016/02/13 16:38:18 by crenault         ###   ########.fr       */
+/*   Updated: 2016/02/13 17:23:22 by crenault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Window.hpp"
 
-// test eigen
+// test cl.hpp
+#include "cl.hpp"
 #include <stdio.h>
+
+#define GLFW_EXPOSE_NATIVE_COCOA
+#define GLFW_EXPOSE_NATIVE_NSGL
+#include "glfw3native.h"
+#include <OpenGL/OpenGL.h>
+
+#if defined (__APPLE__) || defined(MACOSX)
+# define CL_GL_SHARING_EXT "cl_APPLE_gl_sharing"
+#else
+# define CL_GL_SHARING_EXT "cl_khr_gl_sharing"
+#endif
+
+// ///////////
+
+/*// test eigen
 #include <iostream>
 #include "Eigen/Dense"
-// //////////
+// //////////*/
 
 int		main(int argc, char const **argv)
 {
@@ -29,8 +45,70 @@ int		main(int argc, char const **argv)
 	glEnable(GL_DEPTH_TEST);
 	glDepthFunc(GL_LESS);
 
-	// test eigen
+	// test cl.hpp
 
+	cl_int						ret;
+	std::vector<cl::Platform>	platforms;
+
+	ret = cl::Platform::get(&platforms);
+	printf("Number of platforms: %ld\n", platforms.size());
+
+	std::vector<cl::Device>		devices;
+
+	ret = platforms.front().getDevices(CL_DEVICE_TYPE_GPU, &devices);
+	printf("Number of devices GPU on first platform: %ld\n", devices.size());
+
+	std::string					device_name;
+	std::string					device_extensions;
+
+	ret = devices.front().getInfo(CL_DEVICE_NAME, &device_name);
+	printf("Device name: %s\n", device_name.c_str());
+
+	ret = devices.front().getInfo(CL_DEVICE_EXTENSIONS, &device_extensions);
+	if (device_extensions.find(CL_GL_SHARING_EXT) == std::string::npos)
+	{
+		dprintf(2, "This device doesn't support GL/CL sharing...\n");
+		throw std::exception();
+	}
+
+	// Get current CGL Context and CGL Share group
+	CGLContextObj kCGLContext = CGLGetCurrentContext();
+	CGLShareGroupObj kCGLShareGroup = CGLGetShareGroup(kCGLContext);
+
+	// Create CL context properties, add handle & share-group enum
+	cl_context_properties properties[] = {
+		CL_CONTEXT_PROPERTY_USE_CGL_SHAREGROUP_APPLE,
+		(cl_context_properties)kCGLShareGroup,
+		0
+	};
+
+	cl::Context		context = cl::Context::Context(devices, properties, NULL, NULL, &ret);
+
+	if (ret != CL_SUCCESS)
+	{
+		dprintf(2, "Error while trying to create context...\n");
+		throw std::exception();
+	}
+
+	// create vao's
+	// here
+
+	// create opengl buffer here
+	// void glGenBuffers(GLsizei n​, GLuint * buffers​);
+	// void glBindBuffer(GLenum target​, GLuint buffer​);
+	// void glBufferData​(enum target, sizeiptr size, const void *data, enum usage)
+
+	cl::BufferGL::BufferGL(context, CL_MEM_READ_WRITE, /*GLuint bufobj*/0, &ret);
+
+	if (ret != CL_SUCCESS)
+	{
+		dprintf(2, "Error while trying to create buffer from openGL...\n");
+		throw std::exception();
+	}
+
+	// ///////////
+
+	/*// test eigen
 	Eigen::MatrixXd m(2,2);
 
 	m(0,0) = 3;
@@ -38,9 +116,7 @@ int		main(int argc, char const **argv)
 	m(0,1) = -1;
 	m(1,1) = m(1,0) + m(0,1);
 	std::cout << m << std::endl;
-	// printf("%s\n", static_cast<std::ostream&>(std::cout, m));
-
-	// //////////
+	// //////////*/
 
 	while (glfwWindowShouldClose(window.getPtr()) == GL_FALSE)
 	{
